6ef81efa386309b5d33b886a46f612bbf33e032b
Данил Посохов <lyudmila.gartman99@mail.ru>
2020-03-02 17:08:43 +0400

    Add role and user

---
+++ IS.Domain/Model/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS.Domain.Model
+{
+    public class Role
+    {
+        public int ID { get; set; }
+        public string RolePlay { get; set; }
+    }
+}
