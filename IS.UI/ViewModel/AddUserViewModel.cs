45884e54173acc66ea702fd12bffe5bed72f87a2
Данил Посохов <lyudmila.gartman99@mail.ru>
2020-03-03 23:45:42 +0400

    Add NewUserView(model)

---
+++ IS.UI/ViewModel/AddUserViewModel.cs
@@ -0,0 +1,34 @@
+﻿using IS.Domain;
+using IS.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace IS.UI.ViewModel
+{
+    public class AddUserViewModel: Abstract.BindableObject
+    {
+        private User m_Add = new User();
+        public User AddUser
+        { 
+            get=>m_Add; 
+            set 
+            {
+                m_Add = value;
+            }
+        }
+        private readonly Context context;
+        public AddUserViewModel()
+        {
+            context = new Context();
+        }
+        public List<Role> roles { get=>context.Roles.ToList();}
+        public ICommand AddUsers { get => new Command.ActionCommand((obj) => Add(obj)); }
+        public void Add(object obj) 
+        {
+            var user = context.Users.Add(AddUser);
+        }
+    }
+}
