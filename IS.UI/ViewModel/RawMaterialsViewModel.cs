4a13d80e1fdd1dab2313e652fabc2671c4bc9ee6
Данил Посохов <lyudmila.gartman99@mail.ru>
2020-03-07 00:08:20 +0400

    Add RawMaterials

---
+++ IS.UI/ViewModel/RawMaterialsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using IS.Domain;
+using IS.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace IS.UI.ViewModel
+{
+    public class RawMaterialsViewModel: Abstract.BindableObject
+    {
+        readonly Context context;
+        public ObservableCollection<RawMaterial> rawMaterials { get; set; }
+        public RawMaterialsViewModel() 
+        {
+            context = new Context();
+            rawMaterials = new ObservableCollection<RawMaterial>(context.RawMaterials.ToList());
+        }
+    }
+}
