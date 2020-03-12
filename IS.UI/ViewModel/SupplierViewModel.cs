47940d6eed98bd0de8f7c3ac79857b29c4a418b7
Данил Посохов <lyudmila.gartman99@mail.ru>
2020-03-12 04:17:36 +0400

    Remake SupplView

--- IS.UI/ViewModel/SupplierViewModel.cs
+++ IS.UI/ViewModel/SupplierViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace IS.UI.ViewModel
 {
@@ -12,10 +13,28 @@ namespace IS.UI.ViewModel
     {
         readonly Context context;
         public ObservableCollection<Supplier> Suppliers { get; set; }
+        public ObservableCollection<RawMaterialsToOrder> rawMaterialsToOrder { get; set; }
         public SupplierViewModel() 
         {
             context = new Context();
             Suppliers = new ObservableCollection<Supplier>(context.Suppliers.ToList());
+            rawMaterialsToOrder = new ObservableCollection<RawMaterialsToOrder>(context.RawMaterialsToOrder.ToList());
+        }
+        private Supplier m_supplier;
+        public Supplier Supplier
+        {
+            
+            get => m_supplier;
+            set 
+            {
+                m_supplier = value;
+            }
+        }
+        public ICommand AddSuplier { get => new Command.ActionCommand((obj) => AddSupli(obj)); }
+        public void AddSupli(object obj) 
+        {
+            context.Suppliers.Add(m_supplier);
+            context.SaveChanges();
         }
     }
 }
