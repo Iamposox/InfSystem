e0c79c633cdd6c0315d67a1252f652637c0dc8f1
Данил Посохов <lyudmila.gartman99@mail.ru>
2020-03-17 14:22:42 +0400

    Add to function AddInAssortiment

--- IS.UI/ViewModel/AssortmentViewModel.cs
+++ IS.UI/ViewModel/AssortmentViewModel.cs
@@ -5,28 +5,54 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace IS.UI.ViewModel
 {
-    public class AssortmentViewModel:Abstract.BindableObject
+    public class AssortmentViewModel : Abstract.BindableObject
     {
         readonly Context context;
         public ObservableCollection<Assortment> Assortments { get; set; }
-        public List<Product >Product { get; set; }
-        public AssortmentViewModel() 
+        public List<Product> Product { get; set; }
+        public AssortmentViewModel()
         {
             context = new Context();
             Assortments = new ObservableCollection<Assortment>(context.Assortments.ToList());
             Product = new List<Product>(context.Products.ToList());
         }
-        private Product m_SelectedProd;
-        public Product SelectedProd 
+        private Product m_SelectedProd = new Product();
+        public Product SelectedProd
         {
             get => m_SelectedProd;
-            set 
+            set
             {
                 m_SelectedProd = value;
+                Assortment = new Assortment { Product = value, InAssortment=m_Assortiment  };
             }
+        }
+        private int m_Assortiment;
+        public int Assortiment
+        {
+            get => m_Assortiment;
+            set
+            {
+                m_Assortiment = value;
+            }
+        }
+        private Assortment m_Assortment = new Assortment();
+        public Assortment Assortment
+        {
+            get => m_Assortment;
+            set
+            {
+                m_Assortment = value;
+            }
+        }
+        public ICommand AddInAssort { get => new Command.ActionCommand((obj) => AddAssort(obj)); }
+        public void AddAssort(object obj) 
+        {
+            context.Assortments.Add(m_Assortment);
+            context.SaveChanges();
         }
     }
 }
